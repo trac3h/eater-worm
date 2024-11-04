19a0911e586d9000ac8880ebf724492071cb6261
trac3h <154023419+trac3h@users.noreply.github.com>
2024-11-04 18:06:35 -0500

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,125 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "rotting strawberry":return img`
+. . . . . . . d . . . . . . . . 
+. . . . . . d d d . . . d d . . 
+. . . e e e d d d d . d d d . . 
+. . e e e e e e d d d d d . . . 
+. e e e 2 2 e d d d d d d d . . 
+. e e 2 2 e d d d d d d d d d . 
+e e 2 b e e d d d d e d d d . . 
+e e 2 2 e e d d d e e d d d d . 
+e e 2 e e e d d e e e e d d d . 
+e e 2 e e 2 e e e 2 e e e c d . 
+e e 2 e e e e e e e e e e c d . 
+e e e e e e e e 2 e e e e c . . 
+e e e e e e e e e e e e c . . . 
+e e e e e e e e e e e c . . . . 
+e e e e e e c e c c c . . . . . 
+. c c c c c c c . . . . . . . . 
+`;
+            case "image2":
+            case "rotting cherry":return img`
+. . . . . . . . . . . d d d d d 
+. . . . . . . . . d d d d d d d 
+. . . . . . d d d d d d d d d d 
+. . e e e e c d d d d . d d d . 
+. e e d 2 e e c d . . . d d d . 
+e e 2 e e e e e c . . . d d d . 
+e e e e e e e e c . . . d d d . 
+e e e e e e e e e e e e c d d . 
+c e e e e e e e e e d 2 e c d . 
+. c e e e e e e e 2 e e e e c . 
+. . c e e e e e e e e e e e e e 
+. . . e c c e c e e e e e e e e 
+. . . . . . . c e e e e e e e c 
+. . . . . . . c e e e e e e e c 
+. . . . . . . . c e e e e e c . 
+. . . . . . . . . c c c c c . . 
+`;
+            case "image4":
+            case "wolf":return img`
+bbbb........bbbb.................
+cddbb......bbddb.................
+cdddbb....bbdddc.................
+cdbddbccccbddbdc.................
+cdbbddbccbddbbdc.................
+cd3bddddddddb3dc.................
+cd33dddddddd33dc.................
+cdd3dddddddd3ddc.................
+cddddddd11dddddc.................
+cdddffd111dffddc........bbbbbb...
+cdddffd1111ffddc.......cddddddbb.
+cdddd1111111dddc......cddddddddb.
+cddd1111fff11ddc.....cddd111ddddb
+cdddc11fff11ddbc.....cdd11111dddb
+cdddcc111111cdbc.....cd11dd111ddb
+cdddddbb33ccddbcc....cbddbbd11ddc
+cddddddb33cdddddbbccccbbdbbb11ddc
+cdddddddbbddddddddddddddddbb1dddc
+cdddd1111dddddddddddddddddddddddc
+cdddd11111dddddddddddddddddddddc.
+cddd1111111dddddddddddddddddddcc.
+cdd11111111dddddddddddddddddddc..
+cdd111111111ddddddddddddddddddc..
+cbd111111111ddddddddddddddddddc..
+.fd111111111ddddddddddddddddddc..
+.ff111111111ddddddddddddddddddc..
+..fd11111111ddddddddddddddddddc..
+...fd111111ddddddd111111ddddddc..
+...fddd1111dddddd11111111dddddc..
+....fddfffbddddccccccccccbddddc..
+....fddf..fdddc.....fddf.cddddc..
+....fddf..fdddc.....fddf.ccddddc.
+....fddf..fddc.......fdf..ccdddc.
+....fddf..fddc.......fddf..ccddc.
+....fddf..fddc.......fddf...cddc.
+....fddf..fddc......fdddf...cddc.
+...fdddf..fddc......ffff....cddc.
+...fddf..fdddc.............cdddc.
+...ffff..fddcc.............cdddc.
+.........fffc..............cccc..
+`;
+            case "image3":
+            case "good dirt":return img`
+. . . . . . . e e e e . . . . . 
+. . . . . e e d d d e e e . . . 
+. . . . e e e d d d e e e e . . 
+. . . e e e e d d d e e e e e . 
+. . e e e e e e e e e e e e e . 
+. e e e e e e e e e e e e e e e 
+. e e e e e e d d d e e d d d e 
+e e d d d e e d d d e e d d d e 
+e e d d d e e d d d e e d d d e 
+e e d d d e e e e e e e e e e . 
+e e e e e e e e e e e e e e . . 
+e e e e e e e e e e e e e . . . 
+e e e e d d d e e e e . . . . . 
+e e e e d d d e e e . . . . . . 
+. e e e d d d e . . . . . . . . 
+. . e e e e e . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
